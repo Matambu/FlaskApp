27fe8389c67b099bd397185e7d17b6ae46f3ef5e
Matambu <markomatambu@outlook.com>
2024-02-28 00:21:52 +0000

    part 2

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import UserDetailsForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'psMsFJX3K6zwDchi'
 
 posts = [
     {
@@ -19,6 +22,21 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = UserDetailsForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/loginpage")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login Page', form=form)
+
+
 
 
 if __name__ == '__main__':
